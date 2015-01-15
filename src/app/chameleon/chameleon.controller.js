291b650f8ecb68b785e9f03a94631c4ddd58ec53
Craig Richardson <craig.rich@hotmail.co.uk>
2015-01-15 15:17:37 +0000

    Finish POC concept for chameleon

--- src/app/chameleon/chameleon.controller.js
+++ src/app/chameleon/chameleon.controller.js
@@ -1,28 +1,21 @@
 'use strict';
 
 angular.module('ebo')
-    .controller('ChameleonCtrl', function($scope) {
+    .controller('ChameleonCtrl', function($scope, $rootScope) {
 
         var chameleonCtrl = this;
-
+        
         $scope.saveChanges = function(config) {
             chameleonCtrl.saveCSS(config.CSS);
         };
 
         chameleonCtrl.saveCSS = function(cssObject) {
-
             var css = CSSJSON.toCSS(cssObject);
             console.log(css);
 
-
             var style = document.createElement("style");
                 style.appendChild(document.createTextNode(css));
-                document.head.appendChild(style);
-
-            
-
-
-
+                document.head.appendChild(style);            
         };
 
     });
